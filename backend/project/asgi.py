55d76506e4f26a91a5205216cd30b1d547ba5a98
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-10 21:44:32 +0300

    starting in the backend and create an empty project

---
+++ backend/project/asgi.py
@@ -0,0 +1,33 @@
+"""
+ASGI config for project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+from django.urls import path
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # WebSocket chat handler
+    # "websocket": AllowedHostsOriginValidator(
+    #     AuthMiddlewareStack(
+    #         URLRouter([
+    #             path("chat/admin/", AdminChatConsumer.as_asgi()),
+    #             path("chat/", PublicChatConsumer.as_asgi()),
+    #         ])
+    #     )
+    # ),
+})
