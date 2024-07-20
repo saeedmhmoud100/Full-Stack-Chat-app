1fe1a58b7583077e12bac79cde6bc363aab6a4f3
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-21 00:01:20 +0300

    backend: create account manager model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,16 +1,35 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.db import models
-
-
-
 
 
 def get_profile_image_filepath(self, filename):
     return f'profile_image/{self.pk}/{"profile_image.png"}'
 
+
 def get_default_profile_image():
     return "profile_image/default_profile_image.png"
 
-class Account(models.Model):
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        if not username:
+            raise ValueError('The Username must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_admin', True)
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class Account(AbstractBaseUser):
     email = models.EmailField(verbose_name='email', max_length=60, unique=True)
     username = models.CharField(max_length=30, unique=True)
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
@@ -20,7 +39,10 @@ class Account(models.Model):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    profile_image = models.ImageField(max_length=255, upload_to=get_profile_image_filepath, null=True, blank=True, default=get_default_profile_image)
+    profile_image = models.ImageField(max_length=255, upload_to=get_profile_image_filepath, null=True, blank=True,
+                                      default=get_default_profile_image)
+
+    objects = AccountManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
