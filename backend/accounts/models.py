aa549730e84aba8e628e156a74063c6aeefe24cf
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-20 23:59:46 +0300

    backend: create account model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+def get_profile_image_filepath(self, filename):
+    return f'profile_image/{self.pk}/{"profile_image.png"}'
+
+def get_default_profile_image():
+    return "profile_image/default_profile_image.png"
+
+class Account(models.Model):
+    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    profile_image = models.ImageField(max_length=255, upload_to=get_profile_image_filepath, null=True, blank=True, default=get_default_profile_image)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
