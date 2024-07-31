2658b596d27db362c58decadb81e4fc443fcda1e
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-31 19:20:09 +0300

    frontend: update some design in search page to show if user friend or not

--- frontend/src/app/(pages)/(authentecated)/search/page.tsx
+++ frontend/src/app/(pages)/(authentecated)/search/page.tsx
@@ -2,6 +2,9 @@
 
 import Link from "next/link";
 import {useSelector} from "react-redux";
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
+import {Button} from "@mui/material";
 
 export default function SearchPage() {
     const {usersSearch} = useSelector(state => state.user)
@@ -15,12 +18,38 @@ export default function SearchPage() {
             {
                 usersSearch?.map((user) => {
                     return (
-                        <>
-                            <Link href={`/users/${user.id}`} className="md:w-[49%] w-full h-24 px-4 flex gap-3 my-3 items-center hover:bg-gray-300 cursor-pointer transition delay-100 rounded-md">
+                        <div key={user.id} className='md:w-[49%] w-full h-24 px-4 flex items-center hover:bg-gray-300 cursor-pointer transition delay-100 rounded-md'>
+                            <Link href={`/users/${user.id}`} className="w-full h-24 flex gap-3 my-3 items-center ">
                                 <img src={user.profile_image} style={{height:"60px",width:"60px"}}  className='rounded-3xl'/>
                                 <h4>{user.username}</h4>
                             </Link>
-                        </>
+                            <div className='border border-gray-400 px-4 py-2 rounded-md'>
+                                {user.is_friend ? <Button size='large' sx={{
+                                        fontSize: "18px",
+                                        fontWeight: "bold",
+                                        color:"black",
+                                    display: "flex",
+                                    alignItems: "center",
+                                }}>
+                                    <span className='mt-[3px]'>Friend</span>
+                                    <CheckIcon color='success' fontSize='medium' className='mx-2' style={{border:"3px solid #2e7d32",borderRadius:"50%"}}/>
+                                </Button> :
+
+                                    <Button size='large' sx={{
+                                        fontSize: "15px",
+                                        fontWeight: "bold",
+                                        color:"black",
+                                        display: "flex",
+                                        alignItems: "center",
+                                        width:"max-content",
+                                    }}>
+                                        <span className='mt-[3px]'>Not Friend</span>
+                                        <ClearIcon color='error' fontSize='medium' className='mx-2' style={{border:"3px solid #d32f2f",borderRadius:"50%"}}/>
+                                    </Button>
+                                }
+                            </div>
+
+                        </div>
                     )
                 })
             }
