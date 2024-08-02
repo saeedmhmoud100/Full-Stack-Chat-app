e3ee71034cf5e91550ccbecd821b35d5b4d3de03
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-08-02 15:27:37 +0300

    frontend: solve some problems in notifications

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -176,7 +176,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
