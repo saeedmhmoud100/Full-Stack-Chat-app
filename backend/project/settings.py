0ad3bc6f47274e114668280b48a902099d885e26
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-23 23:06:05 +0300

    backend: set debug with false in settings file

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-uxvzlvq_nlpmf!o*xjw9^f051tj!twj)n4&fbus53ehv6emgct"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
