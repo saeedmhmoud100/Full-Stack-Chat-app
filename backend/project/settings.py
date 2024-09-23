4de4d6c723504724cd311574176b2ce48ceccf9b
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-23 23:01:12 +0300

    backend: update database settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': os.environ.get('POSTGRES_DB'),
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'PORT': os.environ.get('5432'),
     }
 }
 
