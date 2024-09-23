613d2e8984a1601de0f2060f673e78ba83c902c9
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-09-23 21:20:20 +0300

    backend: move database to /temp to make it writeable in vercel

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "tmp/db.sqlite3",
     }
 }
 
